7843a60a405a65b784041ab90099c95bbc37e0c6
Alexis Algoud <alexis@atm-consulting.fr>
2015-07-29 21:38:14 +0200

    sans https, impossible d'avoir une reconnaissance permanente

--- js/voice.js.php
+++ js/voice.js.php
@@ -3,8 +3,19 @@
 ?>
 $(document).ready(function () {
 	
-
-	if('webkitSpeechRecognition' in window) {
+	if(window.self === window.top && 'webkitSpeechRecognition' in window) {
+		$('body').css('margin',0);
+		$('body').empty();
+		$('body').append('<iframe id="frame-voice" src="'+document.location.href+'" style="background:#fff; border: 0; width: 100%; height: 100%"></iframe>');
+	    
+	    $('#frame-voice').css({
+	    	position:'absolute'
+	    	,top:0
+	    	,left:0
+	    	,width:window.scrollWidth
+	    	,height:window.scrollHeight
+	    	
+	    });
 	    
         $('body').append('<a href="#" id="speech-button"><img src="<?php echo dol_buildpath('/voice/img/voice-icon.png',1); ?>" /></a>');
         
@@ -12,7 +23,7 @@ $(document).ready(function () {
         
 	    var recognition = new webkitSpeechRecognition();
 	    recognition.lang='fr-FR';
-	    recognition.continuous=false;
+	    recognition.continuous=true;
 	    recognition.interimResults = false;
 	    
 	    btn.click(function(e) {
@@ -40,7 +51,8 @@ $(document).ready(function () {
 	            
 	        }
 	        
-	        
+/*	        btn.find('img').attr("src","<?php echo dol_buildpath('/voice/img/voice-icon-run.png',1); ?>");
+	        recognition.start();*/
 	    }
 	    
 	}
@@ -49,33 +61,37 @@ $(document).ready(function () {
 
 function transcriptionAnalyser(ph) {
 
+	$frame = $('#frame-voice');
+
+	console.log(ph);
+
     var matches = ph.match(/^recherche produit (.+)/i);
     if(matches && matches.length>1) {
         search = matches[1];    
             
-        $('#sallsearchleftp').val(search);
-        $('#sallsearchleftp').closest('form').submit();
+        $frame.contents().find('#sallsearchleftp').val(search);
+        $frame.contents().find('#sallsearchleftp').closest('form').submit();
         
         return 1;     
     }
 
     var matches = ph.match(/^nouvelle facture/i);
     if(matches) {
-        document.location.href="<?php echo dol_buildpath('/compta/facture.php?action=create',1); ?>";
+        $frame.attr('src',"<?php echo dol_buildpath('/compta/facture.php?action=create',1); ?>");
         
         return 1;     
     }
       
     var matches = ph.match(/^nouvelle propale/i);
     if(matches) {
-        document.location.href="<?php echo dol_buildpath('/comm/propal.php?action=create',1); ?>";
+        $frame.attr('src',"<?php echo dol_buildpath('/comm/propal.php?action=create',1); ?>");
         
         return 1;     
     }
       
     var matches = ph.match(/^nouvel événement/i);
     if(matches) {
-        document.location.href="<?php echo dol_buildpath('/comm/action/card.php?action=create',1); ?>";
+        $frame.attr('src',"<?php echo dol_buildpath('/comm/action/card.php?action=create',1); ?>");
         
         return 1;     
     }
@@ -83,12 +99,12 @@ function transcriptionAnalyser(ph) {
     var matches = ph.match(/^déconnect(.*) moi/i);
     
     if(matches) {
-        document.location.href="<?php echo dol_buildpath('/user/logout.php',1); ?>";
+        $frame.attr('src',"<?php echo dol_buildpath('/user/logout.php',1); ?>");
         
         return 1;     
     }
     
-    alert("Commande non reconnue : "+ph);
+    //alert("Commande non reconnue : "+ph);
 
 }
 
